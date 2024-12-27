e2ace9228a211a9c3c8d5c65c458beb730de552c
Przemek <przemek.wojszwillo@gmail.com>
2024-12-27 16:31:02 +0100

    Adds router config, adds consts, enums & types files

---
+++ src/consts/index.ts
@@ -0,0 +1,39 @@
+import type { MenuLink } from '@/types'
+
+export const MAIN_MENU_LIST: MenuLink[] = [
+  {
+    label: 'O Kazusie',
+    uid: 'o-kazusie',
+    url: 'o-kazusie',
+  },
+  {
+    label: 'O Firmie',
+    uid: 'o-firmie',
+    url: 'o-firmie',
+  },
+  {
+    label: 'Czasopisma',
+    uid: 'czasopisma',
+    url: 'czasopisma',
+  },
+  {
+    label: 'Szkolenia',
+    uid: 'szkolenia',
+    url: 'szkolenia',
+  },
+  {
+    label: 'Baza Wiedzy',
+    uid: 'baza-wiedzy',
+    url: 'baza-wiedzy',
+  },
+  {
+    label: 'Kazus TV',
+    uid: 'kazus-tv',
+    url: 'kazus-tv',
+  },
+  {
+    label: 'Kontakt',
+    uid: 'kontakt',
+    url: 'kontakt',
+  },
+]
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export interface MenuLink {
+  label: string
+  url: string
+  uid: string
+}
