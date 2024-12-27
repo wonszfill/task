0dee1f357f8ab5156fe44f0e3f4bf8fec99163d6
Przemek <przemek.wojszwillo@gmail.com>
2024-12-27 16:30:18 +0100

    Initial commit - adds basic app setup from create-vue, adds sass pkg

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import MainNav from './components/MainNav.vue'
+</script>
+
+<template>
+  <MainNav />
+  <RouterView />
+</template>
+
+<style scoped></style>
